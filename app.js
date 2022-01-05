a1bf1b08e043c704fc8058ef00ead6e9816ef11e
Gaurav kar <sauravchandrakar@gmail.com>
2022-01-05 09:06:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const amount =12;
+
+if(amount< 10){
+	console.log("Ohm baba kaam ho gya");
+}
+else
+{
+	console.log(" sahi hai");
+}
+console.log(`BC ${amount}`);
---
+++ prac.js
@@ -0,0 +1,7 @@
+const num1=5;
+const num2=-2;
+
+function sum(num1,num2){
+ console.log(`The sum is ${num1+num2}`)
+}
+sum(num1,num2);
